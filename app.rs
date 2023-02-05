365b32a7ce30850a05823ba22f2fd5ff8b3f7374
coddedwells <wells.mail.13@gmail.com>
2023-02-06 00:45:31 +0300

    📦 NEW: Start learning Rust🔥

---
+++ app.rs
@@ -0,0 +1,10 @@
+//This is a comment that is ignored by rustc
+
+fn main(){
+    println!("Welcome me Universe!!!");
+    println!("I'm a rustacean!!!");
+
+    let mut age:i32 = 34;
+
+    println!("🔥{}",age);
+}
