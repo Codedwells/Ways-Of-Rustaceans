5c203efe86a89a43d885f99580c5c8d6c2a8816c
coddedwells <wells.mail.13@gmail.com>
2023-02-06 09:54:29 +0300

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 //This is a comment that is ignored by rustc
+#![allow(unused)]
 
-fn main(){
+use rand::Rng;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main() {
     println!("Welcome me Universe!!!");
     println!("I'm a rustacean!!!");
 
-    let  age:i32 = 34;
+    let age: i32 = 34;
 
-    println!("🔥{}",age);
+    println!("🔥{}", age);
 }
+
