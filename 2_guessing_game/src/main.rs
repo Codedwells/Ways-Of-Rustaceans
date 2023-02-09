bfe0d6bb0f2543b7bd81cece96e99ad669b28710
coddedwells <wells.mail.13@gmail.com>
2023-02-09 17:42:51 +0300

    📦 NEW: Rust Guessing game

---
+++ 2_guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Secret num is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        // We declate variables using let
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!!".red()),
+            Ordering::Greater => println!("{}", "Too big!!!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win".green());
+                break;
+            }
+        }
+    }
+}
