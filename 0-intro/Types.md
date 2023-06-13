8ec867faa4cb9f4f201b60b77fe01d40eb73f773
codedwells <wells.mail.13@gmail.com>
2023-06-13 12:13:02 +0300

    📦 NEW: tuples

--- 0-intro/Types.md
+++ 0-intro/Types.md
@@ -81,3 +81,53 @@ fn main() {
 }
 
 ```
+
+# Tuples
+
+Are a record of data
+
+```rs
+enum Access {
+    Full,
+}
+
+fn one_two_three() -> (i32, i32, i32) {
+    return (1, 2, 3);
+}
+
+fn main() {
+    let numbers = one_two_three();
+    let (x, y, z) = one_two_three();
+
+    println!("{}", numbers.1);
+
+    let (employee, access) = ("Jake", Access::Full);
+}
+```
+
+## Ativity Solution
+
+```rs
+fn cartesian() -> (i32, i32, i32) {
+    return (2, 5, 8);
+}
+
+fn main() {
+    let (_x, y, _z) = cartesian();
+
+    if y < 5 {
+        println!("Less than five!!");
+    } else if y > 5 {
+        println!("Greater than five!!");
+    } else {
+        println!("Equal to five");
+    }
+
+    match y {
+        4 => println!("Less than five!!"),
+        5 => println!("Equal to five"),
+        _ => println!("Less greater than five!!"),
+    }
+}
+
+```
--- 0-intro/main.rs
+++ 0-intro/main.rs
@@ -1,25 +1,22 @@
-enum Flavor {
-    Orange,
-    Cola,
-}
-
-struct Drinkbox {
-    flavoring: Flavor,
-    fluid_oz: f64,
-}
-
-fn print_flavor(my_box: Drinkbox) {
-    match my_box.flavoring {
-        Flavor::Orange => println!("Orange"),
-        Flavor::Cola => println!("Cola"),
-    }
+fn cartesian() -> (i32, i32, i32) {
+    return (2, 5, 8);
 }
 
 fn main() {
-    let my_box = Drinkbox {
-        flavoring: Flavor::Orange,
-        fluid_oz: 12.35,
-    };
+    let (_x, y, _z) = cartesian();
 
-    print_flavor(my_box);
+    if y < 5 {
+        println!("Less than five!!");
+    } else if y > 5 {
+        println!("Greater than five!!");
+    } else {
+        println!("Equal to five");
+    }
+
+    match y {
+        4 => println!("Less than five!!"),
+        5 => println!("Equal to five"),
+        _ => println!("Less greater than five!!"),
+    }
 }
+ 
