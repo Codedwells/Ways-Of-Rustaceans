cfaed2830af9e8852aac6b623daf2664edd12092
codedwells <wells.mail.13@gmail.com>
2023-06-13 12:32:38 +0300

    📦 NEW: expressions

--- 0-intro/Types.md
+++ 0-intro/Types.md
@@ -131,3 +131,26 @@ fn main() {
 }
 
 ```
+
+# Expresssions
+
+```rs
+
+enum Access {
+    Admin,
+    Manager,
+    User,
+    Guest,
+}
+
+fn main() {
+    let access_level = Access::Guest;
+    let can_access_file = match access_level {
+        Access::Admin => true,
+        _ => false,
+    };
+
+    println!("can access: {}", can_access_file);
+}
+
+```
--- 0-intro/main.rs
+++ 0-intro/main.rs
@@ -1,22 +1,7 @@
-fn cartesian() -> (i32, i32, i32) {
-    return (2, 5, 8);
-}
-
 fn main() {
-    let (_x, y, _z) = cartesian();
+    let my_number = 102;
 
-    if y < 5 {
-        println!("Less than five!!");
-    } else if y > 5 {
-        println!("Greater than five!!");
-    } else {
-        println!("Equal to five");
-    }
+    let checkVal = my_number < 100;
 
-    match y {
-        4 => println!("Less than five!!"),
-        5 => println!("Equal to five"),
-        _ => println!("Less greater than five!!"),
-    }
+    println!("{}", checkVal);
 }
- 
