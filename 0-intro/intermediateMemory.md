8cae12a91f2a207cd094708b3b4b8144f775d972
codedwells <wells.mail.13@gmail.com>
2023-06-14 11:21:08 +0300

    📦 NEW: more of impl

--- 0-intro/intermediateMemory.md
+++ 0-intro/intermediateMemory.md
@@ -106,3 +106,72 @@ fn main() {
     cold.show_temp();
 }
 ```
+
+## More of Impl, structs and enums
+
+```rs
+enum BoxColor {
+    RED,
+    BLUE,
+    GREEN,
+}
+ 
+impl BoxColor {
+    fn print_color(&self) {
+        match self {
+            BoxColor::RED => println!("The color of this box is : Red"),
+            BoxColor::BLUE => println!("The color of this box is : Blue"),
+            BoxColor::GREEN => println!("The color of this box is : Green"),
+        }
+    }
+}
+
+struct Dimensions {
+    length: f64,
+    width: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print_dimensions(&self) {
+        println!("The length is :{}", self.length);
+        println!("The width is :{}", self.width);
+        println!("The depth is :{}", self.depth);
+    }
+}
+
+struct Shippingbox {
+    dimensions: Dimensions,
+    weight: f64,
+    color: BoxColor,
+}
+
+impl Shippingbox {
+    fn new(weight: f64, color: BoxColor, dimensions:Dimensions) -> Self {
+        return Self {
+            weight,
+            dimensions,
+            color,
+        };
+    }
+
+    fn print_box(&self) {
+        self.color.print_color();
+        self.dimensions.print_dimensions();
+        println!("The weight of this box is :{}", self.weight);
+    }
+}
+
+fn main() {
+    let dimensions = Dimensions {
+        length: 34.0,
+        width: 24.0,
+        depth: 14.0,
+    };
+
+    let blue_box = Shippingbox::new(12.0, BoxColor::BLUE, dimensions);
+
+    blue_box.print_box();
+}
+
+```
--- 0-intro/main.rs
+++ 0-intro/main.rs
@@ -1,25 +1,63 @@
-struct Temprature {
-    defrees_f: f64,
+enum BoxColor {
+    RED,
+    BLUE,
+    GREEN,
+}
+ 
+impl BoxColor {
+    fn print_color(&self) {
+        match self {
+            BoxColor::RED => println!("The color of this box is : Red"),
+            BoxColor::BLUE => println!("The color of this box is : Blue"),
+            BoxColor::GREEN => println!("The color of this box is : Green"),
+        }
+    }
 }
 
-// Lower case self  show there is already a temprature declared somewhere
-// Uppercase Self
-impl Temprature {
-    fn freezing() -> Self {
-        return Self { defrees_f: 32.5 };
+struct Dimensions {
+    length: f64,
+    width: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print_dimensions(&self) {
+        println!("The length is :{}", self.length);
+        println!("The width is :{}", self.width);
+        println!("The depth is :{}", self.depth);
+    }
+}
+
+struct Shippingbox {
+    dimensions: Dimensions,
+    weight: f64,
+    color: BoxColor,
+}
+
+impl Shippingbox {
+    fn new(weight: f64, color: BoxColor, dimensions:Dimensions) -> Self {
+        return Self {
+            weight,
+            dimensions,
+            color,
+        };
     }
 
-    fn show_temp(&self) {
-        println!("The overall temp is :{}", self.defrees_f);
+    fn print_box(&self) {
+        self.color.print_color();
+        self.dimensions.print_dimensions();
+        println!("The weight of this box is :{}", self.weight);
     }
 }
 
 fn main() {
-    let hot = Temprature { defrees_f: 99.9 };
+    let dimensions = Dimensions {
+        length: 34.0,
+        width: 24.0,
+        depth: 14.0,
+    };
 
-    // We can access the function using dot notion since we implimented the struct
-    hot.show_temp();
+    let blue_box = Shippingbox::new(12.0, BoxColor::BLUE, dimensions);
 
-    let cold = Temprature::freezing();
-    cold.show_temp();
+    blue_box.print_box();
 }
