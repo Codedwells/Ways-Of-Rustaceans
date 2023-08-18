95eee0e6c13948809a5fe6d3c0535d6930fac5a2
codedwells <wells.mail.13@gmail.com>
2023-08-18 11:38:59 +0300

    New:testing

---
+++ 11_testing/src/lib.rs
@@ -0,0 +1,40 @@
+struct Rectangle {
+    width: u32,
+    length: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, smaller: &Rectangle) -> bool {
+        let volume_large = self.width * self.height * self.length;
+        let volume_small = smaller.width * smaller.height * smaller.length;
+
+        if volume_large > volume_small {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_hold_rect() {
+        let smaller = Rectangle {
+            width: 5,
+            length: 9,
+            height: 4,
+        };
+
+        let larger = Rectangle {
+            width: 9,
+            length: 6,
+            height: 4,
+        };
+
+        assert_eq!(larger.can_hold(&smaller), true);
+    }
+}
